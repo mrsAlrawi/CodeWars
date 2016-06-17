2f06ad65ec1dacc6d0ae224ae1eab8070b665efa
Mihyar Almasalma <eng.mihyear@gmail.com>
2016-06-17 16:10:33 +0300

    this is my solutions for functions in Code Wars

---
+++ Training.js
@@ -0,0 +1,28 @@
+function findNextSquare(sq) {
+  // Return the next square if sq if a perfect square, -1 otherwise
+  if(Math.sqrt(sq)=== Math.floor(Math.sqrt(sq)))
+  return Math.pow(Math.sqrt(sq)+1,2)
+  return -1;
+}
+
+function GetSum(a,b){
+	if(a>b){
+	return GetSum(b,a);
+	}
+	if(a===b){
+		return a;
+	}
+	return a + GetSum(a+1,b);
+}
+
+function getCount(words) {
+	var obj ={vowels : 0, consonants : 0};
+	for (var i=0;i<words.length;i++){
+		if(words[i] === 'i' || words[i] === 'o' || words[i] === 'u' || words[i] === 'e' || words[i]==='a'){
+		obj.vowels++;
+  	}else if(words[i]!== " "){
+	  obj.consonants++;
+	  }
+	}
+  return obj;
+}
