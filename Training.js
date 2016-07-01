d637c43e93fbdb64e4470f08365a82b81ce94748
mihyar-30614 <eng.mihyear@gmail.com>
2016-07-01 21:04:00 +0300

    level 7

--- Training.js
+++ Training.js
@@ -26,7 +26,7 @@ function getCount(words) {
 	}
   return obj;
 }
-
+//My solution
 function squareDigits(num){
 	var string = num.toString();
 	var result = "";
@@ -36,7 +36,42 @@ function squareDigits(num){
 	}
 	return parseInt(result);
 }
-
+// Code war solution
 function squareDigits(num){
   return Number(('' + num).split('').map(function (val) { return val * val;}).join(''));
+}
+// my solution 
+function alphabetPosition(text) {
+var text = text.toLowerCase();
+var string="";
+ for(var i=0; i<text.length; i++){
+ var x = text[i].charCodeAt(0)-96;
+ if(/[a-z]/i.test(text[i]))
+ string+= x+" ";
+ }
+ string= string.slice(0,-1)
+ return string;
+}
+// Code war solution 
+function alphabetPosition(text) {
+  return text
+    .toLowerCase()
+    .replace(/[^a-z]/g,'')
+    .replace(/./g, c => c + " ")
+    .replace(/[a-z]/g, c => c.charCodeAt(0) - 96)
+    .trim();    
+}
+// My soltion 
+var isSquare = function(n){
+		if(n>=0 && Math.sqrt(n)%1 ===0)
+		  return true; 
+		  return false;
+}
+// Code war solution 
+var isSquare = function(n){
+	return Math.sqrt(n)%1 ===0
+}
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '');
 }
