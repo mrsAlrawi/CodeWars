2071ea411efac99dd2fb44c0f5a95f269acc4f74
mihyar-30614 <eng.mihyear@gmail.com>
2016-07-06 13:47:47 +0300

    solved some proplems

--- Training.js
+++ Training.js
@@ -122,4 +122,32 @@ function duplicateEncode(word){
       return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
     })
     .join('');
+};
+
+function reverse(str) {
+	return str.length > 1 ? reverse(str.slice(1)) + str[0] : str;
+}
+
+var employees =  [{ firstName: 'Ollie', lastName: 'Hepburn', role: 'Boss' },
+{ firstName: 'Morty', lastName: 'Smith', role: 'Truck Driver' },
+{ firstName: 'Peter',lastName: 'Ross',role: 'Warehouse Manager' },
+{ firstName: 'Cal', lastName: 'Neil', role: 'Sales Assistant' },
+{ firstName: 'Jesse', lastName: 'Saunders', role: 'Admin' },
+{ firstName: 'Anna', lastName: 'Jones', role: 'Sales Assistant' },
+{ firstName: 'Carmel', lastName: 'Hamm', role: 'Admin' },
+{ firstName: 'Tori', lastName: 'Sparks', role: 'Sales Manager' },
+{ firstName: 'Peter',lastName: 'Jones',role: 'Warehouse Picker' },
+{ firstName: 'Mort',lastName: 'Smith',role: 'Warehouse Picker' },
+{ firstName: 'Anna', lastName: 'Bell', role: 'Admin' },
+{ firstName: 'Jewel', lastName: 'Bell', role: 'Receptionist' },
+{ firstName: 'Colin', lastName: 'Brown', role: 'Trainee' } ];
+
+function findEmployeesRole(name) {
+	for (var i = 0; i < employees.length; i++) {
+		var fullName = employees[i].firstName + ' ' + employees[i].lastName;
+		if (name === fullName){
+			return employees[i].role;
+		}
+	}
+	return 'Does not work here!';
 }
